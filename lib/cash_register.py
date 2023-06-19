5ed4bb5394966327e027afb0fbf7fd404e2fb5ac
Dovid Yarmush <yarmushda@gmail.com>
2023-06-19 16:43:01 -0400

    passing all cash regiter lab tests

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0, total=0):
+        self.discount = discount
+        self.total = total
+        self.items = list()
+        self.latest = 0
+
+    def add_item(self, title, price, quantity=1):
+        self.latest = price * quantity
+        self.total += (price * quantity)
+        self.items.extend([title] * quantity)
+        return self.total
+    
+    def apply_discount(self):
+        if self.discount:
+            discount = self.discount/100 * self.total
+            self.total -= discount
+            return print(f'After the discount, the total comes to ${int(self.total)}.')
+        else:
+            return print("There is no discount to apply.")
+        
+    def void_last_transaction(self):
+        self.total -= self.latest
+
+        
+     
